c04db4291dd8c7ecf1cbbae480f9e02eb39d308f
DimAURA <dimauro.luca@studenti.salesianisesto.it>
2026-06-20 09:22:27 +0200

    Update server.js

--- render/server.js
+++ render/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, 'public')));
 let databaseTemporaneo = [];
 app.post('/salva', (req, res) => {
     databaseTemporaneo.push(req.body);
@@ -15,7 +15,7 @@ app.get('/dati', (req, res) => {
     res.json(databaseTemporaneo);
 });
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server in esecuzione sulla porta ${PORT}`);
